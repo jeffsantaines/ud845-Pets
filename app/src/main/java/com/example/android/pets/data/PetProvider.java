c9f91a0dc1e7d885a5cb35aa52d599dba0d65058
Jeff <jeffsantaines@gmail.com>
2017-10-08 15:48:45 +1300

    UriMatcher instantiated

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -2,6 +2,7 @@ package com.example.android.pets.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -14,6 +15,17 @@ import android.support.annotation.Nullable;
 public class PetProvider extends ContentProvider {
 
     public static final String LOG_TAG = PetProvider.class.getSimpleName();
+
+    public static final int PET = 100;
+    public static final int PETS = 101;
+
+    public static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY,PetContract.PATH_PETS,PET);
+
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY,PetContract.PATH_PETS +"/#",PETS);
+    }
 
     /**
      * Initialize the provider and the db helper object
